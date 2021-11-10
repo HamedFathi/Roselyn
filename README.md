bd91aa17e4332cf17338132e8fbe6496eda119f9
hamedfathi <hamedfathi@outlook.com>
2021-11-11 02:21:08 +0330

    feat: functionalities added.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Roselyn
+![compiler](https://user-images.githubusercontent.com/8418700/141161804-348696a6-efd2-4cb6-8938-b435578ed2b4.png)
+
+<hr/>
+<div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
---
+++ Roselyn/RoslynExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using RoslynQuoter;
+
+namespace Roselyn
+{
+    public static class RoslynExtensions
+    {
+        public static string FormatCsharpSourceCode(this string sourceCode)
+        {
+            var tree = CSharpSyntaxTree.ParseText(sourceCode);
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var workspace = new AdhocWorkspace();
+            var syntax = Formatter.Format(root, workspace);
+            return syntax.GetText().ToString();
+        }
+        public static SyntaxNode FormatCsharpSourceCode(this SyntaxNode syntaxNode)
+        {
+            var workspace = new AdhocWorkspace();
+            return Formatter.Format(syntaxNode, workspace);
+        }
+        public static string ToRoslynAPI(this string source, bool useDefaultFormatting = true, bool openParenthesisOnNewLine = false
+                        , bool closingParenthesisOnNewLine = false, bool removeRedundantModifyingCalls = false, bool shortenCodeWithUsingStatic = false)
+        {
+            var quoter = new Quoter()
+            {
+                UseDefaultFormatting = useDefaultFormatting,
+                ShortenCodeWithUsingStatic = shortenCodeWithUsingStatic,
+                RemoveRedundantModifyingCalls = removeRedundantModifyingCalls,
+                OpenParenthesisOnNewLine = openParenthesisOnNewLine,
+                ClosingParenthesisOnNewLine = closingParenthesisOnNewLine
+            };
+            return quoter.Quote(source).ToString();
+        }
+    }
+}
